987820c5d1ee89b983b80f3bb3180b7bec8fa947
LucienHyc <huany918@newschool.edu>
2017-05-01 01:28:50 +0200

    Initial Commit

---
+++ ocean/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ocean
+//
+//  Created by Lucien on 5/1/17.
+//  Copyright © 2017 Lucien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
